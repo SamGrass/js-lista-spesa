982c89f15af5cafea0d5668e930e39fbee4065e2
Samgrass <samuelegrassetti01@gmail.com>
2024-06-11 16:32:33 +0200

    add "listaSpesa" and container element assignment

---
+++ js/myscript.js
@@ -0,0 +1,6 @@
+// creare elemento container lista
+let containerLista = document.querySelector("ul");
+// creare un array che contenga la lista della spesa
+const listaSpesa = ["pane", "banane", "marmellata", "pesce", "pere", "pesche"];
+// creare un ciclo che estrapoli la lista
+
