aa63b13491f5f6aabc2cbb2baa84099bfe1f4a15
Samgrass <samuelegrassetti01@gmail.com>
2024-06-11 16:36:18 +0200

    add while loop to represent the list in the html FINE ESERCIZIO

--- js/myscript.js
+++ js/myscript.js
@@ -3,4 +3,12 @@ let containerLista = document.querySelector("ul");
 // creare un array che contenga la lista della spesa
 const listaSpesa = ["pane", "banane", "marmellata", "pesce", "pere", "pesche"];
 // creare un ciclo che estrapoli la lista
-
+let i = 0
+while (i < listaSpesa.length) {
+    const itemSpesa = listaSpesa[i];
+    // creo un elemento li ripetuto tanto quanto gli elementi della lista
+    let itemLista = `<li>${itemSpesa}</li>`
+    i++
+    // inserisco nell'ul
+    containerLista.innerHTML += itemLista;
+}
